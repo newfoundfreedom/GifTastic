5398d77b622f07a3b3b93ebaea8f8d183cb18cd6
joel <joel@YAVIN-IV.local>
2017-03-11 23:42:30 -0500

    finished logic / added image / fleshed out CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,109 +1,84 @@
-// Initial array of gifs
-let gifs = ["He Man", "GI Joe", "Transformers", 'Super Friends', "She Ra", "Gummi Bears",'Care Bears', 'DuckTales',
-    'Scooby Do', 'Strawberry Shortcake', 'Power Puff Girls', 'The Flintstones', 'Spiderman', 'Justice League',
-    'Inspector Gadget', 'Mr T', 'Tom and Jerry'];
-
-// displayGifInfo function re-renders the HTML to display the appropriate content
-function displayGifInfo() {
-
-    let userSearch = $(this).attr("data-name")+' cartoon';
-    console.log(userSearch);
-    let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userSearch + '&limit=10&api_key=dc6zaTOxFJmzC';
-
-    // Creates AJAX call for the specific gif button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (response) {
-        console.log(response);
-        $('#gifs-view').empty();
-        $.each(response.data, function (index) {
-            let item = response.data[index],
-                still = item.images.fixed_height_still.url,
-                animated = item.images.fixed_height.url,
-                rating = item.rating;
+// GLOBAL VARIABLES
+let shows = ['Super Friends', 'GI Joe', 'Thundercats','He Man', 'Transformers',
+    'Inspector Gadget', 'Bugs Bunny', 'Tom and Jerry', 'DuckTales',
+    'The Flintstones', 'She Ra', 'Care Bears', 'Strawberry Shortcake'];    // Initialize array of tv-show names
 
 
-            $('<div>')
-                .attr('id', 'gif-card')
-                .attr('display', 'inline-block')
-                .addClass('left')
-                .prependTo('#gifs-view');
+// DISPLAY GIFS FUNCTION
+function displayGifs() {                                                   // Function to display gifs from giphy.com based on user input
 
-            $('<figure>')
-                .attr('id', 'fig')
-                .append('<figcaption>Rated: ' + rating + '</figcaption>')  // Creates an element to have the rating displayed
-                .appendTo('#gif-card');
+    let userSearch = $(this).data('name') + ' cartoon',                    // extract string value from button + word 'cartoon' and assign that to the userSearch variable
+        resultqty = 10,                                                    // define the quantity of results desired
+        apikey = '&api_key=dc6zaTOxFJmzC';                                 // assign the giphy api key
 
-            $('<img>')
-                .attr('src', still)
-                .data("type", {
-                    still: still,
-                    anim: animated
-                })
-                .prependTo('#fig');
+    let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userSearch + '&limit=' + resultqty + apikey;  // build the AJAX url string utilizing the userSearch, result quantity, & api keys
+
+    $.ajax({                                                  // AJAX call
+        url: queryURL,                                        // AJAX call URL definition
+        method: "GET"                                         // AJAX method
+    }).done(function (response) {                             // when JSON object response received from giphy.com - do the following:
+        $('#gifs-view').empty();                              //   clear the 'gifs-view' div in the DOM
+        $.each(response.data, function (index) {              //   for each item in the response object's data child
+            let item = response.data[index],                  //     capture item
+                still = item.images.fixed_height_still.url,   //     capture the still gif url
+                animated = item.images.fixed_height.url,      //     capture the animated gif url
+                rating = item.rating;                         //     capture the gif rating
+
+            $('<figure>')                                     //   create a figure element
+                .attr('id', 'fig')                            //     assign the id 'fig' to the figure element
+                .append('<figcaption>Rated: ' + rating + '</figcaption>')  //     place the gif rating into the figcaption element and add to the figure
+                .prependTo('#gifs-view');                     //     add figure to the 'gifs-view' div
+
+            $('<img>')                                        //   create an image element
+                .attr('src', still)                           //     initialize the image source with the still gif
+                .addClass('gif-img')
+                .data("type", {                               //     in the data attribute
+                    still: still,                             //       set still gif url to data-type-still
+                    anim: animated                            //       set animated gif url to data-type-anim
+                })                                            //
+                .prependTo('#fig');                           //     add image to precede the figcaption within the figure element
         });
 
+        $('img').on('click', function () {                    //   CLICK EVENT listener:  when a gif image is clicked - do the following:
+            let currentImg = $(this).attr('src'),             //      capture the displayed gif url
+                stillImg = $(this).data('type').still,        //      capture the url for the still gif (held in data-type-still attribute)
+                animImg = $(this).data('type').anim;          //      capture the url for the animated gif (held in the data-typ-anim attribute)
 
-        $('img').on('click', function(){
-
-            let currentImg = $(this).attr('src'),
-                stillImg = $(this).data('type').still,
-                animImg = $(this).data('type').anim;
-
-            if (currentImg == stillImg) {
-                $(this).attr('src', animImg);
+            if (currentImg == stillImg) {                     //   compare current image url to the still image url
+                $(this).attr('src', animImg);                 //      if they are the same - set the animated gif url as the displayed image source
             }
-
-            else {
-                $(this).attr('src',stillImg);
+            else {                                            //      if the current image url is not equal to the still image url - then it must be the animated url
+                $(this).attr('src', stillImg);                //        set they still image url as the displayed image source
             }
         })
     })
 }
 
+// RENDER BUTTONS FUNCTION
+function renderButtons() {                       // function to display list of shows as buttons
+    $('#buttons-view').empty();                  // clear the 'buttons-view' div
 
-// Function for displaying gif data
-function renderButtons() {
-
-    // Deletes the gifs prior to adding new gifs
-    // (this is necessary otherwise you will have repeat buttons)
-    let buttonsArea = $('#buttons-view');
-
-    buttonsArea.empty();
-    // Loops through the array of gifs
-    for (let i = 0; i < gifs.length; i++) {
-
-        // Then dynamicaly generates buttons for each gif in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        let a = $("<button>");
-        // Adds a class of gif to our button
-        a.addClass("gif");
-        // Added a data-attribute
-        a.attr("data-name", gifs[i]);
-        // Provided the initial button text
-        a.text(gifs[i]);
-        // Added the button to the buttons-view div
-        buttonsArea.append(a);
+    for (let i = 0; i < shows.length; i++) {     // for each show listed listed in the shows array - do the following:
+        $("<button>")                            //   create a button element
+            .addClass('show')                    //   add 'show' class to the button
+            .data('name', shows[i])              //   add show name in the data-name attribute
+            .text(shows[i])                      //   display the show name as the button text
+            .appendTo('#buttons-view');          //   add button to the 'buttons-view' div
     }
 }
 
-// This function handles events where the add gif button is clicked
-$("#add-gif").on("click", function (event) {
-    event.preventDefault();
-    // This line of code will grab the input from the textbox
-    let gif = $('#gif-input').val().trim();
-
-
-    // The gif from the textbox is then added to our array
-    gifs.push(gif);
-
-    // Calling renderButtons which handles the processing of our gif array
-    renderButtons();
+// NEW SHOW CLICK EVENT LISTENER
+$("#add-show").on("click", function (event) {    // CLICK EVENT listener:  when the 'add-gif' button is clicked - do the following
+    event.preventDefault();                      //   stop the default action for the submit button
+    let input = $('#show-input');                //   capture the input field and set to 'input' variable
+    let newshow = input.val().trim();            //   capture and clean the text entered into input field by user
+    shows.push(newshow);                         //   add text captured to the array of shows
+    $(input).val('');                            //   reset the input field
+    renderButtons();                             //   call the renderButtons function to display shows (included one just added) as buttons
 });
 
-// Adding click event listeners to all elements with a class of "gif"
-$(document).on("click", ".gif", displayGifInfo);
+// DISPLAY GIFS CLICK EVEN LISTENER
+$(document).on("click", ".show", displayGifs);    // Add click listener to all buttons with the 'show' class - which will run the displayGifs function
 
-// Calling the renderButtons function to display the intial buttons
-renderButtons();
+// PAGE LOAD EVENT
+renderButtons();  // initial call to the renderButtons function on page load
